23291120b309bd9e6194d6423ecd3fcab32cc0b2
Davinchi-x7 <dufgvjdu@gmail.com>
2023-02-16 09:18:38 +0300

    first django

--- djangoProject1/urls.py
+++ djangoProject1/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.about, name='contact us'),
+    path('accounts/', views.about, name='accounts')
+
 ]
